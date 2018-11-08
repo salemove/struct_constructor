3c929d1477543ddf43afb88dc04551d798491c8f
Alexei Mikhailov <amikhailov83@gmail.com>
2018-11-08 14:07:39 +0200

    Release v0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule StructConstructor.Mixfile do
   def project do
     [
       app: :struct_constructor,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       build_embedded: Mix.env() == :prod,
